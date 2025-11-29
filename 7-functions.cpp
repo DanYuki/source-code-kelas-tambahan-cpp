cef4d85617250ce2e3716f6859ebcd2f96c6170a
DanYuki <daniyusuf.muhammad@gmail.com>
2025-11-29 12:09:56 +0700

    updated the function problem

--- 7-functions.cpp
+++ 7-functions.cpp
@@ -4,9 +4,20 @@ using namespace std;
 
 // TODO: Bagaimana cara mendefinisikan sebuah function?
 
+// TODO: Buat function bernama 'greeting' untuk mencetak string
+// "Halo, nama saya {nama}"
+
 // TODO: Menggunakan persamaan matematika untuk menghitung hasil (2 hingga 3 variabel)
 
-// TODO: 
+// TODO: Ubah logic FizzBuzz ke dalam sebuah functions
+
+// TODO: Buat sebuah function yang dapat menghitung diskon dari sebuah harga barang
+// function bernama 'hitungDiskon' dengan 2 parameter yaitu 'harga' & 'diskon'
+// kembalikan angka double sebagai hasilnya.
+
+// TODO: Mengapa sebelum mendefinisikan function perlu menggunakan tipe data?
+// TODO: Apa itu parameters dalam function?
+
 
 int main() {
     
